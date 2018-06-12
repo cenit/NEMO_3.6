845c65748eaa5dff9aff0c1acdf9b411ab42daed
mathiot <mathiot@4aad9cc9-4d31-0410-b1e8-ee312aa4b1ec>
2018-06-12 15:36:37 +0000

    correcting bug introduced this morning (tmask(ji,jj) instead of tmask(ji,jj,1))

--- NEMO/releases/release-3.6/NEMOGCM/NEMO/LIM_SRC_2/limsbc_2.F90
+++ NEMO/releases/release-3.6/NEMOGCM/NEMO/LIM_SRC_2/limsbc_2.F90
@@ -406,8 +406,10 @@ CONTAINS
          DO jj = 2, jpjm1                             !* ice stress over ocean WITHOUT a ice-ocean rotation angle
             DO ji = fs_2, fs_jpim1
                !                                            ! ocean area at u- & v-points
-               zfrldu  = ( frld(ji,jj) * tmask(ji,jj) + frld(ji+1,jj) * tmask(ji+1,jj  ) ) / MAX(1.0_wp,tmask(ji,jj)+tmask(ji+1,jj  ))
-               zfrldv  = ( frld(ji,jj) * tmask(ji,jj) + frld(ji,jj+1) * tmask(ji  ,jj+1) ) / MAX(1.0_wp,tmask(ji,jj)+tmask(ji  ,jj+1))
+               zfrldu = ( frld(ji,jj) * tmask(ji,jj,1) + frld (ji+1,jj) * tmask(ji+1,jj,1) ) &
+                  &     / MAX( 1.0_wp , tmask(ji,jj,1) + tmask(ji+1,jj,1))
+               zfrldv = ( frld(ji,jj) * tmask(ji,jj,1) + frld (ji,jj+1) * tmask(ji,jj+1,1) ) &
+                  &     / MAX( 1.0_wp , tmask(ji,jj,1) + tmask(ji,jj+1,1))
                !                                            ! quadratic drag formulation without rotation
                !                                            ! using instantaneous surface ocean current
                zutau_ice = 0.5 * ( tmod_io(ji,jj) + tmod_io(ji+1,jj) ) * ( u_ice(ji,jj) - pu_oce(ji,jj) )
